1172a0c1665ed5a315ae1af52d094e0f00f86343
Rinava <asdasd5056@gmail.com>
2021-01-14 12:32:27 -0300

    nav en admin añadida

--- segundo/src/app/admin/admin-routing.module.ts
+++ segundo/src/app/admin/admin-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NavComponent } from './components/nav/nav.component';
 import { ProductFormComponent } from './components/product-form/product-form.component';
 
-const routes: Routes = [{ path: 'create', component: ProductFormComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: NavComponent,
+    children: [{ path: 'create', component: ProductFormComponent }], // HACE QUE LOS HIJOS TAMBIEN TENGAN LA BARRA DE NAV
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
