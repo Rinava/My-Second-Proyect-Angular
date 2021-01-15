31df99021edb03ad80b65ea348658a154ab15b06
Rinava <asdasd5056@gmail.com>
2021-01-15 10:05:35 -0300

    get product

--- segundo/src/app/core/services/products/products.service.ts
+++ segundo/src/app/core/services/products/products.service.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Product } from '../../models/product.model';
-
+import {environment} from './../../../../environments/environment';
 @Injectable({
   providedIn: 'root',
 })
 export class ProductsService {
   constructor(private http: HttpClient) {}
 
-  getAllProducts() {
-    return this.http.get<Product[]>('http://platzi-store.herokuapp.com/products');
+  getAllProducts(){
+    return this.http.get<Product[]>(`${environment.url_api}/products/`);
   }
 
   getProduct(id: string) {
-    return this.http.get('https://platzi-store.herokuapp.com/products/${id}'); // concateno la id
+    return this.http.get<Product>(`${environment.url_api}/products/${id}`); // concateno la id
   }
 }
