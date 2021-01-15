6b3ba656371ab164ba5d92d032287b0e3ff87754
Rinava <asdasd5056@gmail.com>
2021-01-15 12:48:44 -0300

    form-product creado

--- segundo/src/app/admin/admin-routing.module.ts
+++ segundo/src/app/admin/admin-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormProductComponent } from './components/form-product/form-product.component';
 import { NavComponent } from './components/nav/nav.component';
 import { ProductFormComponent } from './components/product-form/product-form.component';
 import { ProductsListComponent } from './components/products-list/products-list.component';
@@ -11,6 +12,7 @@ const routes: Routes = [
     children: [
       { path: 'create', component: ProductFormComponent },
       { path: 'products', component: ProductsListComponent },
+      { path: 'products/create', component: FormProductComponent },
     ], // HACE QUE LOS HIJOS TAMBIEN TENGAN LA BARRA DE NAV
   },
 ];
