f4da699684a5e243fde605001961db049515298e
Rinava <asdasd5056@gmail.com>
2021-01-18 12:29:29 -0300

    contador carrito cambiado con pipe sin subscribe

--- segundo/src/app/shared/components/header/header.component.ts
+++ segundo/src/app/shared/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators'; // queremos transformar el valor que nos llega en uno nuevo
 import { CartService } from './../../../core/services/cart.service';
 
@@ -8,14 +9,12 @@ import { CartService } from './../../../core/services/cart.service';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  total = 0;
+  total$: Observable<number>; // hago que total sea un observable
   constructor(private cartService: CartService) {
-    this.cartService.cart$
-      .pipe(map((products) => products.length)) // quiero transformar los productos en su largo
-      .subscribe((total) => {
-        // me llega el total ya transformado gracias al pipe
-        this.total = total;
-      });
+    this.total$ = this.cartService.cart$.pipe(
+      map((products) => products.length) // quiero transformar los productos en su largo
+    ); // ya no nos subscribimos, guardamos el valor en un observable, un flujo de datos, nos subscribimos desde template
+    // ahora angular se encarga de la suscripcion, no tenemos que desuscribirnos nosotros
   }
 
   ngOnInit(): void {}
