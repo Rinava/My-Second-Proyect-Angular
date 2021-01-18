23660b03b940fd4858a103c802d52a086081204d
Rinava <asdasd5056@gmail.com>
2021-01-18 09:55:17 -0300

    estilo form product y validaciones

--- segundo/src/app/admin/components/form-product/form-product.component.ts
+++ segundo/src/app/admin/components/form-product/form-product.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+import { ProductsService } from 'src/app/core/services/products/products.service';
+import { MyValidators } from 'src/app/utils/validators';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-form-product',
   templateUrl: './form-product.component.html',
@@ -8,19 +10,34 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class FormProductComponent implements OnInit {
   form: FormGroup;
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private productsService: ProductsService,
+    private router: Router
+  ) {
     this.buildForm();
   }
 
   ngOnInit(): void {}
 
+  saveProduct(event: Event) {
+    event.preventDefault(); // porque por default recarga la pagina y no queremos eso
+    if (this.form.valid) {
+      const product = this.form.value;
+      this.productsService.createProduct(product).subscribe((newProduct) => {
+        this.router.navigate(['./admin/products']);
+      });
+    }
+  }
+
   private buildForm() {
     this.form = this.formBuilder.group({
       id: ['', [Validators.required]],
       title: ['', [Validators.required]],
-      price: [0, [Validators.required]],
+      price: [0, [Validators.required, MyValidators.isPriceValid]],
       image: '',
       description: ['', [Validators.required]],
     });
   }
+
 }
